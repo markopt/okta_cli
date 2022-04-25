aec18c2fef752ab960ebaa4388496ec36f328854
Mark Huntley <markhuntley9@gmail.com>
2022-04-25 12:41:47 -0700

    Initial commit for okta cli

---
+++ bin/index.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const yargs = require("yargs");
+const axios = require("axios");
+
+const options = yargs
+	.usage("Usage: -n <name>")
+ 	.option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
+ 	.argv;
+
+const greeting = `Hello, ${options.name}!`;
+
+console.log(greeting);
+
+console.log("Here's a random joke for you");
+
+// const url = "https://icanhazdadjoke.com/";
+
+// axios.get(url, { headers: { Accept: "application/json" } })
+//  .then(res => {
+//    console.log(res.data.joke);
+// });
+
+	
+
+
+
+// const chalk = require("chalk");
+// const boxen = require("boxen");
+
+// const greeting = chalk.white.bold("Hello!");
+
+// const boxenOptions = {
+// 	padding: 1,
+// 	margin: 1,
+// 	borderStyle: "round",
+// 	borderColor: "green",
+// 	backgroundColor: "#555555"
+
+// };
+
+// const msgBox = boxen(greeting, boxenOptions);
+
+
+// console.log("Hello!");
